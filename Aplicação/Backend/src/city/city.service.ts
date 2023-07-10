508b7349727b1e6d30e7a09766c0d1ff0147653b
oliveeiralucas <lucas.oliveira@edu.unifil.br>
2023-07-10 00:33:36 -0300

    feat: city(controller, module e service) + armazenamento em cache para performance

--- Aplicação/Backend/src/city/city.service.ts
+++ Aplicação/Backend/src/city/city.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { CityEntity } from './entities/city.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager/dist';
 
 @Injectable()
-export class CityService {}
+export class CityService {
+  constructor(
+    @InjectRepository(CityEntity)
+    private readonly cityRepository: Repository<CityEntity>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
+
+  async getAllCitiesByStateId(stateId: number): Promise<CityEntity[]> {
+    const citiesCache: CityEntity[] = await this.cacheManager.get(`cities_${stateId}`);
+
+    if (citiesCache) {
+      return citiesCache;
+    }
+
+    const cities = await this.cityRepository.find({
+      where: {
+        stateId,
+      },
+    });
+
+    await this.cacheManager.set(`cities_${stateId}`, cities);
+    return cities;
+  }
+}
