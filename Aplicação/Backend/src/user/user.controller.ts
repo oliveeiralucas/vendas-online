246a973d096db5175a1c7e541e17b56d8fcef176
oliveeiralucas <lucas.oliveira@edu.unifil.br>
2023-07-13 02:06:18 -0300

    feat: retornando address cadastrado pelo user

--- Aplicação/Backend/src/user/user.controller.ts
+++ Aplicação/Backend/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, Get, UsePipes, ValidationPipe, Param } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
 import { UserEntity } from './entities/user.entity';
@@ -16,5 +16,10 @@ export class userController {
   @Get()
   async getAllUser(): Promise<ReturnUserDto[]> {
     return (await this.userService.getAllUser()).map((UserEntity) => new ReturnUserDto(UserEntity));
+  }
+
+  @Get('/:userId')
+  async getUserById(@Param('userId') userId: number): Promise<ReturnUserDto> {
+    return new ReturnUserDto(await this.userService.getUserByIdUsingRelations(userId));
   }
 }
